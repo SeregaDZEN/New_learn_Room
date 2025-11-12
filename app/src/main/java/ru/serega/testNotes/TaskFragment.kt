094bf329b10a769cc82f93e696c2d1831ad2f9fc
СерёгаБелый <fezarikus@gmail.com>
2025-11-12 15:33:27 +0300

    change frag, del obj, shape size,name package

--- app/src/main/java/ru/serega/testNotes/TaskFragment.kt
+++ app/src/main/java/ru/serega/testNotes/TaskFragment.kt
@@ -1,4 +1,4 @@
-package ru.netology.project_notes_room
+package ru.serega.testNotes
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,20 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 
 
-class TaskFragment : Fragment(R.layout.fragment_task) {
-
+class TaskFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_task, container, false)
-
-
-    }
-
-    companion object {
-
     }
 }
